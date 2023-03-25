d18ec8baa6c419643a254ce4ff5f4108cb000224
wacedungeoner <wacedungeon@gmail.com>
2023-03-25 17:33:26 +0800

    Relocate build script out of workflows
    Also add first iteration of fix_filenames script

---
+++ scripts/build.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+directory="../resources" # /resources/
+maxsize=2097152 # 2 GB limit
+
+mkdir ../build
+
+for subdir in "$directory"/*; do
+
+  if [ ! -d "$subdir" ]; then # Skip files for example README.md
+    continue
+  fi
+
+  subdir_name=$(basename "$subdir")
+
+  zip -r "../build/$subdir_name.zip" "$subdir"
+  filesize=$(stat -c%s "../build/$subdir_name.zip")
+
+  if (( filesize > maxsize )); then # Surpassed 2 GB limit
+
+    rm ../build/$subdir_name.zip
+
+    if [ ! -d "$subdir/y11" || ! -d "$subdir/y12" ]; then # Won't be able to reasonably break it up any smaller
+      echo "$subdir was too large to be zipped. Try downloading it with https://download-directory.github.io/" > ../build/_FAILED_$subdir_name.txt
+      continue
+    fi
+
+    zip -r "../build/${subdir_name}_y11.zip" "$subdir/y11"
+    zip -r "../build/${subdir_name}_y12.zip" "$subdir/y12"
+    filesize_y11=$(stat -c%s "../build/${subdir_name}_y11.zip")
+    filesize_y12=$(stat -c%s "../build/${subdir_name}_y12.zip")
+
+    if (( filesize_y11 > maxsize || filesize_y12 > maxsize )); then # Even breaking it up smaller wasn't enough
+      rm ../build/${subdir_name}_y11.zip
+      rm ../build/${subdir_name}_y12.zip
+      echo "$subdir was too large to be zipped. Try downloading it with https://download-directory.github.io/" > ../build/_FAILED_$subdir_name.txt
+    fi
+
+  fi
+
+done
---
+++ scripts/fix_filenames.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+flags="nv"
+
+find ../resources \
+-type f \
+-exec sh -c "mv -$flags"$' "$1" "$(echo $1 \
+| sed \"s/\\ _\\ /,\\ /g\" \
+| sed \"s/\\([^\\ ]\\)+\\([^\\ ]\\)/\\1\\ \\2/g\" \
+| sed \"s/\\([^\\ ]\\)+\\([^\\ ]\\)/\\1\\ \\2/g\" \
+| sed \"s/_/\\ /g\" \
+| sed \"s/\\ \\{2,\\}/\\ /g\" \
+| sed \"s/\\([^\\.\\/]\\)\\.\\{2,\\}/\\1\\./g\" \
+| sed \"s/\\.[A-z0-9]\\+\\(\\.[A-z0-9]\\+\\)$/\\1/\" \
+| sed \"s/([0-9]\\+)\\(\\.[A-z0-9]\\+\\)$/\\1/\" \
+| sed \"s/\\ -\\ Copy\\(\\.[A-z0-9]\\+\\)$/\\1/\" \
+)"' \
+sh {} \;
+
+
+# change 'a _ b' to 'a, b'
+# change 'a+b' to 'a '
+# same again, only once only replaces half
+# change '_' to ' '
+# change '   ' to ' '
+# change '...' to '.', but not if in ../ or similar
+# change 'a.doc.pdf' to 'a.pdf'
+# change 'a(1).pdf' to 'a.pdf'
+# change 'a - Copy.pdf' to 'a.pdf'
