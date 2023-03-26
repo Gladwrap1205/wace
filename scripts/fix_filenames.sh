e38a49a274efda3261d29b1b14b73ade21f502fd
wacedungeoner <wacedungeon@gmail.com>
2023-03-26 13:24:40 +0800

    Add another case
    'a .pdf' to 'a.pdf'

--- scripts/fix_filenames.sh
+++ scripts/fix_filenames.sh
@@ -2,7 +2,7 @@
 
 flags="nv"
 
-find ../resources \
+find . \
 -type f \
 -exec sh -c "mv -$flags"$' "$1" "$(echo $1 \
 | sed \"s/\\ _\\ /,\\ /g\" \
@@ -12,18 +12,20 @@ find ../resources \
 | sed \"s/\\ \\{2,\\}/\\ /g\" \
 | sed \"s/\\([^\\.\\/]\\)\\.\\{2,\\}/\\1\\./g\" \
 | sed \"s/\\.[A-z0-9]\\+\\(\\.[A-z0-9]\\+\\)$/\\1/\" \
-| sed \"s/([0-9]\\+)\\(\\.[A-z0-9]\\+\\)$/\\1/\" \
+| sed \"s/([0-9]\\+)\\ \\?\\(\\.[A-z0-9]\\+\\)$/\\1/\" \
+| sed \"s/\\ \\(\\.[A-z0-9]\\+\\)$/\\1/\" \
 | sed \"s/\\ -\\ Copy\\(\\.[A-z0-9]\\+\\)$/\\1/\" \
 )"' \
 sh {} \;
 
 
 # change 'a _ b' to 'a, b'
-# change 'a+b' to 'a '
+# change 'a+b' to 'a b'
 # same again, only once only replaces half
 # change '_' to ' '
 # change '   ' to ' '
 # change '...' to '.', but not if in ../ or similar
 # change 'a.doc.pdf' to 'a.pdf'
 # change 'a(1).pdf' to 'a.pdf'
+# change 'a .pdf' to 'a.pdf'
 # change 'a - Copy.pdf' to 'a.pdf'
